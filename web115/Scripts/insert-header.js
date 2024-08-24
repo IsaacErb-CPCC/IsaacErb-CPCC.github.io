3b6f051ea37af8d228f8b046d8b90c9c12641d96
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-08-24 00:05:32 -0400

    Fully applied new insert-header.js script.

---
+++ web115/Scripts/insert-header.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const PAGES = [
+	{filename: 'index.html', title: 'Home'},
+	{filename: 'contract.html', title:'Course-Contract'},
+	{filename: 'introductions.html', title: 'Self-Introduction'},
+	{filename: 'brand.html', title: 'Branding'}];
+
+for (let index = 0; index < PAGES.length; index++) {
+	const pair = PAGES[index];
+	document.write(`<li><a href="${pair.filename}">${pair.title}</a></li>`)
+}
