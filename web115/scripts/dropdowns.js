209275a95fe0394ffea29c80636de2c8b49c1773
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-09-21 17:56:26 -0400

    Attempting to add dropdown menu for FizzBuzz-page-links in header, 24.

--- web115/scripts/dropdowns.js
+++ web115/scripts/dropdowns.js
@@ -4,6 +4,9 @@ const FIZZ_BUTTON = document.getElementById("fizz-drop");
 const FIZZ_MENU = document.getElementById("fizzbuzz-links");
 const FIZZ_STATE = window.getComputedStyle(FIZZ_MENU);
 
+const DROPDOWN_LIST = [
+	{button: FIZZ_BUTTON, menu: FIZZ_MENU, status: FIZZ_STATE}];
+
 function toggleMenu(menuElem, menuState) {
 	if (menuState.getPropertyValue("display") === "none") {
 		menuElem.setAttribute("style", "display: block");	//If menu is hidden, set it to be shown.
@@ -17,3 +20,14 @@ function toggleFizz() {
 	toggleMenu(FIZZ_MENU, FIZZ_STATE);
 	return;
 }
+
+function closeOnUnfocus() {
+	let anyFocus = false;
+	for (let ind = 0; ind < DROPDOWN_LIST.length; ind++) {
+
+	}
+}
+
+window.addEventListener("click" () => {
+
+})
