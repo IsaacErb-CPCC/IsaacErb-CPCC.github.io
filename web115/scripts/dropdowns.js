71642eabe129b83662a13144238a786a6ec66492
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-09-22 09:42:47 -0400

    Attempting to add dropdown menu for FizzBuzz-page-links in header, 25.

--- web115/scripts/dropdowns.js
+++ web115/scripts/dropdowns.js
@@ -4,9 +4,6 @@ const FIZZ_BUTTON = document.getElementById("fizz-drop");
 const FIZZ_MENU = document.getElementById("fizzbuzz-links");
 const FIZZ_STATE = window.getComputedStyle(FIZZ_MENU);
 
-const DROPDOWN_LIST = [
-	{button: FIZZ_BUTTON, menu: FIZZ_MENU, status: FIZZ_STATE}];
-
 function toggleMenu(menuElem, menuState) {
 	if (menuState.getPropertyValue("display") === "none") {
 		menuElem.setAttribute("style", "display: block");	//If menu is hidden, set it to be shown.
@@ -21,13 +18,19 @@ function toggleFizz() {
 	return;
 }
 
-function closeOnUnfocus() {
-	let anyFocus = false;
-	for (let ind = 0; ind < DROPDOWN_LIST.length; ind++) {
+//@	THE BELOW IS FOR ADDING A FEATURE FOR CLOSING ON A CLICK
+//	OUTSIDE OF THE MENU.
 
-	}
-}
+// const DROPDOWN_LIST = [
+// 	{button: FIZZ_BUTTON, menu: FIZZ_MENU, status: FIZZ_STATE}];
 
-window.addEventListener("click" () => {
+// function closeOnUnfocus() {
+// 	let anyFocus = false;
+// 	for (let ind = 0; ind < DROPDOWN_LIST.length; ind++) {
+// 		if (DROPDOWN_LIST.button)
+// 	}
+// }
 
-})
+// window.addEventListener("click" () => {
+
+// })
