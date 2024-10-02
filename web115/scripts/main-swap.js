062b0841d0a890ccca7d7b6abfb6f1ded52780b2
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-10-02 18:15:02 -0400

    Experimenting with one-page format, with JS swapping out <main>, 4.

--- web115/scripts/main-swap.js
+++ web115/scripts/main-swap.js
@@ -1,19 +1,36 @@
 "use strict";
 
-class PageContents {
-	constructor(filename) {
-		// let request = fetch(`https://IsaacErb-CPCC.github.io/web115/${filename}.html`);
-		this.contents = fetch(`https://IsaacErb-CPCC.github.io/web115/${filename}.html`).then((response) => response.text());
-	}
-}
+/*
+  ! NOTICE: This script copies the structure of the MDN
+    "fetch-text" example at https://github.com/mdn/dom-examples/blob/main/fetch/fetch-text/index.html.
+*/
 
 const MAIN = document.querySelector("main");
+const HOME_URL = "home.html";
+const CONTENT_LINKS = document.querySelectorAll("nav a");
 
-const HOME = new PageContents("home");
-const BRAND = new PageContents("brand");
-
-function swapMainContents(pageObject) {
-	MAIN.innerHTML = pageObject.contents;
+for (const link of CONTENT_LINKS) {
+	link.onclick = (contentRequest) => {
+		contentRequest.preventDefault();
+		const DATA_URL = contentRequest.target.getAttribute("data-page");
+		swapMainContents(DATA_URL);
+	};
 }
 
-swapMainContents(HOME);
+function swapMainContents(dataURL) {
+	console.log(dataURL);	//@DEBUG-FEATURE!
+	const DATA_REQUEST = new Request(dataURL);
+
+	fetch(DATA_REQUEST)
+		.then((response) => {
+			return response.text();
+		})
+		.then((text) => {
+			MAIN.innerHTML = text;
+		})
+		.catch((error) => {	//@DEBUG-FEATURE!
+			MAIN.innerText = error.message;
+		});
+}
+
+swapMainContents(HOME_URL);
