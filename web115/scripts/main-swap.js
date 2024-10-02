9dc7c67516b5626efcedbcf790d511fd55c824ff
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-10-02 17:22:56 -0400

    Experimenting with one-page format, with JS swapping out <main>, 2.

--- web115/scripts/main-swap.js
+++ web115/scripts/main-swap.js
@@ -2,15 +2,15 @@
 
 class PageContents {
 	constructor(filename) {
-		this.contents = fetch(`https://IsaacErb-CPCC.github.io/web115/${filename}.html`)
-			.then (request => request.text());
+		let request = fetch(`https://IsaacErb-CPCC.github.io/web115/${filename}.html`);
+		this.contents = request.text();
 	}
 }
 
 const MAIN = document.querySelector("main");
 
 const HOME = new PageContents("home");
-const BRAND = new PageContents("brand")
+const BRAND = new PageContents("brand");
 
 function swapMainContents(pageObject) {
 	MAIN.innerHTML = pageObject.contents;
