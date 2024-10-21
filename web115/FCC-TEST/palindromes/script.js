ba8de97749f71aa828d8334e88ae69f2a818fbec
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-10-21 13:32:55 -0400

    Adding directory with the files for JADS Palindromes named to work on FCC.

---
+++ web115/FCC-TEST/palindromes/script.js
@@ -0,0 +1,23 @@
+const inputLoc = document.querySelector("#text-input");
+const outputLoc = document.querySelector("#result");
+const isAlphaNumeric = /^[a-z0-9]+$/;
+
+function palinCheck() {
+	let inText = inputLoc.value;
+	if (inText === "") {
+		window.alert("Please input a value");
+	} else {
+		let inCharUnfiltered = inText.toLowerCase().split("");
+		let inChar = inCharUnfiltered.filter(char => char.match(isAlphaNumeric));
+		let inReverse = [...inChar];
+		inReverse.reverse();
+		let isPalin = true;
+		for (let i = 0; isPalin && i < inChar.length; i++) {
+			const normChar = inChar[i], revChar = inReverse[i];
+			isPalin = (normChar === revChar);
+		}
+
+		message = isPalin ? "is a palindrome":"is not a palindrome";
+		outputLoc.innerText = `"${inText}" ${message}.`;
+	}
+}
