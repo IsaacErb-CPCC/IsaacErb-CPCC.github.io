8d1933a3ec26e20733bca1898347cdb68ba335c9
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-10-24 01:10:36 -0400

    Fixing implementation of main-swap.js, 8.

--- web115/scripts/main-swap.js
+++ web115/scripts/main-swap.js
@@ -43,7 +43,7 @@ function swapContentScript(dataURL) {
 
 	const NEW_SCRIPT = document.createElement('script');
 	NEW_SCRIPT.id = 'content-script';
-	NEW_SCRIPT.defer = true;
+	NEW_SCRIPT.defer = "true";
 	NEW_SCRIPT.src = `scripts/${scriptURL}`;
 	document.head.prepend(NEW_SCRIPT);
 	return;
