5751a02c28fd826b6e00a0e52d1140a5ece60232
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-11-01 12:02:48 -0400

    Working on FCC-assignment hub-pages, 1.

--- web115/scripts/main-swap.js
+++ web115/scripts/main-swap.js
@@ -17,7 +17,9 @@ const FILE_INFO = [
 	{url: "fizzbuzz1", script: "fizz-buzz1.js", title: "FizzBuzz 1"},
 	{url: "fizzbuzz2", script: "fizz-buzz2.js", title: "FizzBuzz 2"},
 	{url: "fizzbuzz3", script: "fizz-buzz3.js", title: "FizzBuzz 3"},
-	{url: "fizzbuzz4", script: "fizz-buzz4.js", title: "FizzBuzz 4"}
+	{url: "fizzbuzz4", script: "fizz-buzz4.js", title: "FizzBuzz 4"},
+	{url: "FCC-TEST/rwd-hub", script: "none.js", title: "FCC RWD"},
+	{url: "FCC-TEST/jads-hub", script: "none.js", title: "FCC JADS"}
 ];
 
 function getLastVisited() {
