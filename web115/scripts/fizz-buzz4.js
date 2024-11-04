c45e87ab46d9103f0909f6450e3b11d12c700840
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-11-03 20:38:38 -0500

    Attempting to fix elements failing to load in time to be discovered by scripts of class .content-script, 11.

--- web115/scripts/fizz-buzz4.js
+++ web115/scripts/fizz-buzz4.js
@@ -1,5 +1,118 @@
 "use strict";
 
+//> Defining functions.
+function isMultiple(numToCheck, factor) {
+	let answer = (numToCheck % factor === 0);
+	return answer;
+}
+
+function tabulateLinenum(lineNum) {
+	const DIGITS = lineNum.toString().length;
+	let displayNum;
+
+	switch (DIGITS) {
+		case 3:
+			displayNum = `${lineNum}. `;
+			break;
+		case 2:
+			displayNum = `0${lineNum}. `;
+			break;
+		case 1:
+			displayNum = `00${lineNum}. `;
+			break;
+	}
+
+	return displayNum;
+}
+
+function setDefaults() {
+	FACTOR_1_INPUT.value = firstFactor;
+	FACTOR_2_INPUT.value = secondFactor;
+	FACTOR_3_INPUT.value = thirdFactor;
+	WORD_1_INPUT.value = firstWord;
+	WORD_2_INPUT.value = secondWord;
+	WORD_3_INPUT.value = thirdWord;
+	LINELIMIT_INPUT.value = lineLimit;
+
+	return;
+}
+
+function configuredFizzBuzz(factorTextPairs) {
+	OUTPUT_1_LOC.innerHTML = "";	//Clearing text of output-element, so repeated presses don't just keep appending.
+
+	//> FizzBuzz Loop:
+	for (let lineNum = 0; lineNum < lineLimit; lineNum++) {	//(lineLimit) here is a global variable.
+		let message = "Robostatus: ";
+
+		for (const IND in factorTextPairs) {
+			const PAIR = factorTextPairs[IND];
+			if (isMultiple(lineNum, PAIR.factor)) {
+				message += PAIR.text+", ";
+			}
+		}
+
+		if (message === "Robostatus: ") {	//If no words were added:
+			message += "Normal.";
+		} else {
+			message = message.slice(0,-2);	//Removing comma and space at end.
+			message = `${message}.`;
+		}
+
+		let outLine = `${tabulateLinenum(lineNum)}${message}<br>`;
+
+		OUTPUT_1_LOC.innerHTML += outLine;
+	}
+
+	return;
+}
+
+//-	getInputs() sets most of the global variables to their
+//	current values from the webpage, and returns the
+//	concatenated pieces of the user's input name after a bit of
+//	processing.
+function getInputs() {
+	firstFactor = FACTOR_1_INPUT.value;
+	secondFactor = FACTOR_2_INPUT.value;
+	thirdFactor = FACTOR_3_INPUT.value;
+	firstWord = WORD_1_INPUT.value;
+	secondWord = WORD_2_INPUT.value;
+	thirdWord = WORD_3_INPUT.value;
+	lineLimit = LINELIMIT_INPUT.value;
+
+	if (lineLimit > LINE_MAX) {
+		window.alert(`Number of lines cannot be greater than ${LINE_MAX}.`);
+		return null;
+	}
+
+	let first = NAME_FIRST_INPUT.value;
+	let mid = NAME_MIDDLE_INPUT.value;
+	let last = NAME_LAST_INPUT.value;
+
+	if (mid.slice(-1) !== ".") {
+		mid += ".";
+	}
+	let full = `${first} ${mid} ${last}`;
+
+	return full;
+}
+
+function doIO() {
+	let name = getInputs();
+	if (name === null) {
+		return;
+	}
+	NAME_LOC.innerHTML = `${EXCALM_INV}Welcome to Lil'RoboCo, ${name}!`;
+
+	let fizzbuzzInput = [
+		{factor: firstFactor, text: firstWord},
+		{factor: secondFactor, text: secondWord},
+		{factor: thirdFactor, text: thirdWord}
+	];
+	configuredFizzBuzz(fizzbuzzInput);
+
+	return;
+}
+
 window.addEventListener("DOMContentLoaded", () => {
 	//> Defining global variables.
 	var SUBMIT_BUTTON = document.getElementById("submission");
@@ -28,120 +141,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	var EXCALM_INV = "&#161;";
 	var LINE_MAX = 1000;
-
-
-	//> Defining functions.
-	function isMultiple(numToCheck, factor) {
-		let answer = (numToCheck % factor === 0);
-		return answer;
-	}
-
-	function tabulateLinenum(lineNum) {
-		const DIGITS = lineNum.toString().length;
-		let displayNum;
-
-		switch (DIGITS) {
-			case 3:
-				displayNum = `${lineNum}. `;
-				break;
-			case 2:
-				displayNum = `0${lineNum}. `;
-				break;
-			case 1:
-				displayNum = `00${lineNum}. `;
-				break;
-		}
-
-		return displayNum;
-	}
-
-	function setDefaults() {
-		FACTOR_1_INPUT.value = firstFactor;
-		FACTOR_2_INPUT.value = secondFactor;
-		FACTOR_3_INPUT.value = thirdFactor;
-		WORD_1_INPUT.value = firstWord;
-		WORD_2_INPUT.value = secondWord;
-		WORD_3_INPUT.value = thirdWord;
-		LINELIMIT_INPUT.value = lineLimit;
-
-		return;
-	}
-
-	function configuredFizzBuzz(factorTextPairs) {
-		OUTPUT_1_LOC.innerHTML = "";	//Clearing text of output-element, so repeated presses don't just keep appending.
-
-		//> FizzBuzz Loop:
-		for (let lineNum = 0; lineNum < lineLimit; lineNum++) {	//(lineLimit) here is a global variable.
-			let message = "Robostatus: ";
-
-			for (const IND in factorTextPairs) {
-				const PAIR = factorTextPairs[IND];
-				if (isMultiple(lineNum, PAIR.factor)) {
-					message += PAIR.text+", ";
-				}
-			}
-
-			if (message === "Robostatus: ") {	//If no words were added:
-				message += "Normal.";
-			} else {
-				message = message.slice(0,-2);	//Removing comma and space at end.
-				message = `${message}.`;
-			}
-
-			let outLine = `${tabulateLinenum(lineNum)}${message}<br>`;
-
-			OUTPUT_1_LOC.innerHTML += outLine;
-		}
-
-		return;
-	}
-
-	//-	getInputs() sets most of the global variables to their
-	//	current values from the webpage, and returns the
-	//	concatenated pieces of the user's input name after a bit of
-	//	processing.
-	function getInputs() {
-		firstFactor = FACTOR_1_INPUT.value;
-		secondFactor = FACTOR_2_INPUT.value;
-		thirdFactor = FACTOR_3_INPUT.value;
-		firstWord = WORD_1_INPUT.value;
-		secondWord = WORD_2_INPUT.value;
-		thirdWord = WORD_3_INPUT.value;
-		lineLimit = LINELIMIT_INPUT.value;
-
-		if (lineLimit > LINE_MAX) {
-			window.alert(`Number of lines cannot be greater than ${LINE_MAX}.`);
-			return null;
-		}
-
-		let first = NAME_FIRST_INPUT.value;
-		let mid = NAME_MIDDLE_INPUT.value;
-		let last = NAME_LAST_INPUT.value;
-
-		if (mid.slice(-1) !== ".") {
-			mid += ".";
-		}
-		let full = `${first} ${mid} ${last}`;
-
-		return full;
-	}
-
-	function doIO() {
-		let name = getInputs();
-		if (name === null) {
-			return;
-		}
-		NAME_LOC.innerHTML = `${EXCALM_INV}Welcome to Lil'RoboCo, ${name}!`;
-
-		let fizzbuzzInput = [
-			{factor: firstFactor, text: firstWord},
-			{factor: secondFactor, text: secondWord},
-			{factor: thirdFactor, text: thirdWord}
-		];
-		configuredFizzBuzz(fizzbuzzInput);
-
-		return;
-	}
 
 
 	//> Setting up default values.
