cdd8e1c8b73ef346feafb7caee52b7cda7635ab6
IsaacErb-Professional <isaac.n.erb@gmail.com>
2024-11-12 06:39:02 -0500

    Trying to figure out one-page assignment, 3.

--- web115/scripts/namari-link.js
+++ web115/scripts/namari-link.js
@@ -2,6 +2,6 @@
 
 const NAMARI_LINK = document.getElementById("to-namari-onepage");
 
-NAMARI_LINK.addEventListener(click, () => {
+NAMARI_LINK.addEventListener("click", () => {
 	window.open("https://isaacerb.github.io/web115/namari-onepage/index.html", "_blank");
 });
